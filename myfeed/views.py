7001ae04ab8c357f80167369c858ebd4625a4451
FELONYANGO <felixonyango951@gmail.com>
2023-02-11 22:48:05 +0300

    updated published

--- myfeed/views.py
+++ myfeed/views.py
@@ -7,6 +7,8 @@ def post_list(request):
     return render(request,'myfeed/post/list.html',{'post':post})
 
 def post_retrieve(request,year,month,day,posts):
-    posts = get_object_or_404(Post,slug=Post, status='published',publish_year=year,publish_month=month)
+    posts = get_object_or_404(Post,slug=Post, 
+                    status='published',publish_year=year,
+                    publish_month=month)
 
-    return render(request,myfeed/post/retrieve.html,{'posts':posts}
+    return render(request,myfeed/post/retrieve.html,{'posts':posts})
